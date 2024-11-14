ffa539255b0a0add8cca4f402d2e8f4f550e2a56
Lapis <45414333+Lapis256@users.noreply.github.com>
2024-11-15 04:02:59 +0900

    Bump version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
         const val name = "FlightSneakPatches"
         const val description = "A mod that patches actions requiring sneaking, added by some mods, to function correctly while flying.\n\nAttribution:\nIcons made by rukanicon ( https://www.flaticon.com/authors/rukanicon ) from https://www.flaticon.com/"
         const val license = "MIT"
-        const val version = "1.1.0"
+        const val version = "1.2.0"
         const val group = "io.github.lapis256"
         const val author = "Lapis256"
         const val repositoryUrl = "https://github.com/Lapis256/FlightSneakPatches"
